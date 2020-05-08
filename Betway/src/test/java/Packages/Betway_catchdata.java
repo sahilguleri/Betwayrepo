28acc023144eea91de3abea06e2734f481890faa
sahilguleri <shivsahilguleri@yahoo.com>
2020-05-08 09:00:05 +0530

    added testNG plugin and added properties file

---
+++ Betway/src/test/java/Packages/Betway_catchdata.java
@@ -0,0 +1,13 @@
+package Packages;
+
+import org.testng.annotations.Test;
+
+public class Betway_catchdata {
+	
+	@Test
+	public void sample()
+	{
+		System.out.println("this is sample data");
+	}
+
+}
